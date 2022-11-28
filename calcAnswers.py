3c4a9ad2eff463f4c37b6a70195970716e5545f5
Mar Ibarra <43395194+kiwigi@users.noreply.github.com>
2022-11-28 00:53:51 -0700

    setting up file

---
+++ calcAnswers.py
@@ -0,0 +1,10 @@
+import json
+import mathplotlib as plt
+
+with open('nodes_all.json') as file:
+    data = json.load(file)
+
+# Is there a correlation between versing a diverse pool of opponents and a player's win rate?
+
+
+# How does a player's age & nationality affect their win rate?
